c09502bae9583ca8d13ff8a044fbc70af6a29e92
istiakiraz <istiakiraz@gmail.com>
2025-04-30 00:59:53 +0600

    added input filed

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router';
+import Navbar from '../components/Navbar';
 
 const root = () => {
     return (
         <>
-            <h1 className='text-4xl font-thin text-center text-cyan-600 mt-8' >Explore email pass Auth</h1>
+            <Navbar></Navbar>
             <Outlet></Outlet>
         </>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import Root from "../src/layouts/Root.jsx"
+import Root from "./layouts/Root.jsx"
+import LogIn from './components/LogIn.jsx'
+
 import { createBrowserRouter, RouterProvider } from "react-router";
 import Home from "./components/Home.jsx";
+import Registretion from "./components/Registretion.jsx";
 
 const router = createBrowserRouter([
   {
@@ -14,6 +17,14 @@ const router = createBrowserRouter([
       {
         index: true,
         Component: Home
+      },
+      {
+        path: 'login',
+        Component: LogIn
+      },
+      {
+        path: "register",
+        Component: Registretion
       }
     ]
   },
